2288450cc35cc9057dd21e36a75b2d5015e4f4fc
Brad <BradleyLoganSherman@gmail.com>
2023-12-15 20:35:28 -0700

    added an oversized longbow file for dkdw

--- SimpleDNDScripts/TheGreatOverSizedLongbow.py
+++ SimpleDNDScripts/TheGreatOverSizedLongbow.py
@@ -12,16 +12,16 @@ for i in range(sample):
 
 
 totalHits = 0
-totalHitsGWM = 0
+totalHitsGOLB = 0
 averageHits = 0
-averageHitsGWM = 0
-strMod = 4
-profMod = 3
-damageDie = 1
+averageHitsGOLB = 0
+strMod = 4 # Keothi Str
+profMod = 3 # Keothi Prof
+damageDie = 6
 damage = 0
-damageGWM = 0
-attackModGWM = -5
-damageModGWM = 10
+damageGOLB = 0
+attackModGOLB = -5
+damageModGOLB = 10
 
 
 for i in range(25):
@@ -29,7 +29,7 @@ for i in range(25):
     for j in range(sample):
         if dice1[j] + strMod + profMod >= (i+1):
             totalHits += 1
-            damage += int((random.random()*100) % damageDie + 1) + strMod
+            damage += int((random.random()*100) % damageDie + 1) + int((random.random()*100) % damageDie + 1) + strMod
         else:
             totalHits += 0
     averageHits = (totalHits/sample) * 100
@@ -37,23 +37,23 @@ for i in range(25):
     print('{:>20}'.format("  Damage: " + str(round(damage, 1))), end='')
 
     for j in range(sample):
-        if dice1[j] + strMod + profMod + attackModGWM >= (i + 1):
-            totalHitsGWM += 1
-            damageGWM += int((random.random() * 100) % damageDie + 1) + strMod + damageModGWM
+        if dice1[j] + strMod + profMod + attackModGOLB >= (i + 1):
+            totalHitsGOLB += 1
+            damageGOLB += int((random.random()*100) % damageDie + 1) + int((random.random()*100) % damageDie + 1) + int((random.random()*100) % damageDie + 1) + int((random.random()*100) % damageDie + 1) + strMod
         else:
-            totalHitsGWM += 0
-    averageHitsGWM = (totalHitsGWM / sample) * 100
-    print('{:>30}'.format("  Chance to hit GWM: %" + str(round(averageHitsGWM, 1))), end='')
-    print('{:>22}'.format("  Damage GWM: " + str(round(damageGWM, 1))), end='')
+            totalHitsGOLB += 0
+    averageHitsGOLB = (totalHitsGOLB / sample) * 100
+    print('{:>30}'.format("  Chance to hit GOLB: %" + str(round(averageHitsGOLB, 1))), end='')
+    print('{:>22}'.format("  Damage GOLB: " + str(round(damageGOLB, 1))), end='')
 
-    print('{:>35}'.format("  Additional GWM damage: " + str(round(damageGWM - damage, 1))), end='')
+    print('{:>35}'.format("  Additional GOLB damage: " + str(round(damageGOLB - damage, 1))), end='')
     print(' ' + str(i+1))
 
     damage = 0
-    damageGWM = 0
+    damageGOLB = 0
 
     totalHits = 0
-    totalHitsGWM = 0
+    totalHitsGOLB = 0
 
 
 # take the attack modifier + 11 and that is about the tipping point: attack bonus 5. 5+11 = 16-17 is the tipping point.
